c9565260808faa8b8deec8348144046005b6943a
fanshilong@ysstech.com <fanshilong@ysstech.com>
2017-12-07 11:18:58 +0800

    search page

--- pages/query/query.js
+++ pages/query/query.js
@@ -5,9 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
+
     date: '{{new Date()}}',
     startdate: '开始时间',
     enddate: '结束时间',
+    listData: [
+      {"time": "2012-01-01", "name": "name1", "road": "alipay", "num": "0123456789", "money": "456.00", "status": "success"},
+      {"time": "2012-01-02", "name": "name2", "road": "alipay", "num": "0123456789", "money": "123.12", "status": "success"},
+      {"time": "2012-01-03", "name": "name3", "road": "wechat", "num": "0123456789", "money": "789.01", "status": "backoff"},
+      {"time": "2012-01-04", "name": "name4", "road": "alipay", "num": "0123456789", "money": "0.12", "status": "success"},
+      {"time": "2012-01-05", "name": "name5", "road": "alipay", "num": "0123456789", "money": "45678.21", "status": "backoff"},
+      {"time": "2012-01-06", "name": "name6", "road": "wechat", "num": "0123456789", "money": "6.00", "status": "success"},
+      { "time": "2012-01-01", "name": "name1", "road": "alipay", "num": "0123456789", "money": "456.00", "status": "success" },
+      { "time": "2012-01-02", "name": "name2", "road": "alipay", "num": "0123456789", "money": "123.12", "status": "success" },
+      { "time": "2012-01-03", "name": "name3", "road": "wechat", "num": "0123456789", "money": "789.01", "status": "backoff" },
+      { "time": "2012-01-04", "name": "name4", "road": "alipay", "num": "0123456789", "money": "0.12", "status": "success" },
+    ]
+  },
+
+  // 交易状态选择
+  radioChange: function(e){
+    console.log(e.detail.value)
   },
 
   // 开始时间
@@ -15,15 +33,17 @@ Page({
     this.setData({
       startdate: e.detail.value
     })
+    console.log(this.data.startdate)
   },
   // 结束时间
   endtime: function (e) {
     this.setData({
       enddate: e.detail.value
     })
+    console.log(this.data.enddate)    
   },
 
-  // 确定时间范围
+  // 确定搜索条件
   confirm: function(){
     
   },
